d886670808d4e77c413a37e0bba9cca794612e49
Nem <vincent.r.greene@gmail.com>
2023-09-26 13:26:58 +0100

    Updated dimensions

--- HelloWorld/MainGame.cpp
+++ HelloWorld/MainGame.cpp
@@ -2,9 +2,9 @@
 #define PLAY_USING_GAMEOBJECT_MANAGER
 #include "Play.h"
 
-int DISPLAY_WIDTH = 640;
-int DISPLAY_HEIGHT = 360;
-int DISPLAY_SCALE = 2;
+int DISPLAY_WIDTH = 1280;
+int DISPLAY_HEIGHT = 720;
+int DISPLAY_SCALE = 1;
 
 // The entry point for a PlayBuffer program
 void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
