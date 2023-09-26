7322b482f13fb6f85eab4132365ed8ac7cfb7b4f
Max <max@65bls.com>
2023-09-26 14:47:37 +0100

    new comments

--- HelloWorld/Maths.cpp
+++ HelloWorld/Maths.cpp
@@ -1,29 +1,29 @@
 #include "Maths.h"
 
-Vec2::Vec2()
+Vec2::Vec2()//empty constructor overload
 {
 	x = 0;
 	y = 0;
 }
 
-Vec2::Vec2(float _x, float _y)
+Vec2::Vec2(float _x, float _y)//x and y co-ord constructor
 {
 	x = _x;
 	y = _y;
 }
 
-Vec2::Vec2(const Vec2& other)
+Vec2::Vec2(const Vec2& other)//make vector from a different vector
 {
 	x = other.x;
 	y = other.y;
 }
 
-Vec2 Vec2::operator+(const Vec2& other)
+Vec2 Vec2::operator+(const Vec2& other) //adding vectors
 {
 	return Vec2(this->x + other.x, this->y + other.y);
 }
 
-Vec2 Vec2::operator-(const Vec2& other)
+Vec2 Vec2::operator-(const Vec2& other) // taking away vector
 {
 	Vec2 result;
 	result.x = this->x - other.x;
@@ -31,17 +31,28 @@ Vec2 Vec2::operator-(const Vec2& other)
 	return result;
 }
 
-Vec2 Vec2::operator*(const float other)
+Vec2 Vec2::operator*(const float other) //multiplying vector by a const.
 {
 	return Vec2(this->x * other, this->y * other);
 }
 
-float Vec2::mag(Vec2 _Vector)
+float Vec2::mag(Vec2 _Vector) //returns the length of the vector
 {
 	float X = _Vector.GetX();
 	float Y = _Vector.GetY();
 	return (float)sqrt(X * X + Y * Y);
 }
+
+/*
+ *	_x = GetX()/mag(vector);
+ *	_y = GetY()/mag(Vector);
+ *	Vec2 UnitVector(_x,_y);
+ *
+ *	Movement = UnitVector * Speed;
+ *	NewPosition = OldPosition + Movement;
+ *
+ */
+
 
 float Vec2::GetX()
 {
