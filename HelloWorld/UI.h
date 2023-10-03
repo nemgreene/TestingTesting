aac68d0e5037ffe8b6d2b6edda4dee41e16d689a
Max <max@65bls.com>
2023-10-03 10:42:17 +0100

    New UI features

---
+++ HelloWorld/UI.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+#include "Play.h"
+#include "Utilities.h"
+
+void handleUI(int _playerHealth, int DISPLAY_WIDTH, int DISPLAY_HEIGHT);
+
+
+//class UI
+//{
+//public:
+//	UI(int DISPLAY_WIDTH, int DISPLAY_HEIGHT);
+//	void handleUI(int _playerHealth, int DISPLAY_WIDTH, int DISPLAY_HEIGHT);
+//private:
+//	
+//	std::string _spriteName;
+//	//int UIBackground;
+//	//int UIDashBar;
+//	//int UIHealth1;
+//	//int UIHealth2;
+//	//int UIHealth3;
+//	//int UIHealth4;
+//};
+
